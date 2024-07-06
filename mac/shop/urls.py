efe191f02800ebbec677b0e326eb3bd0eff62a02
laxmi2541 <rnxlaxmi@gmail.com>
2024-07-06 23:57:15 +0530

    shop related changes

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = {
     path('Shop/', views.index, name="ShopHome"),
     path('about/', views.about, name="AboutUs"),
-    #path('contact/', views.contact, name="ContactUs"),
-    #path('tracker', views.tracker, name="TrackingStatus"),
-    #path('search/', views.search, name="Search"),
-    #path('productview/', views.productView, name="search"),
+    path('contact/', views.contact, name="ContactUs"),
+    path('tracker', views.tracker, name="TrackingStatus"),
+    path('search/', views.search, name="Search"),
+    path('productview/', views.productview, name="productive"),
     path('checkout/', views.checkout, name="Checkout"),
 }
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,11 +1,30 @@
-import django.shortcuts
+from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return django.shortcuts.render(request, 'shop/index.html')
+    return render(request, 'shop/index.html')
+
+
 def about(request):
-    return django.shortcuts.render(request, 'shop/about.html')
+    return HttpResponse("We are at about")
 
 
-def checkout():
-    return None
+def contact(request):
+    return HttpResponse("We are at contact")
+
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+
+def search(request):
+    return HttpResponse("We are at search")
+
+
+def productview(request):
+    return HttpResponse("We are at product view")
+
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
